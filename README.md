b819c88e2931f10e4b008adf6a1b90fb635fd65a
Solia26 <161400353+Solia26@users.noreply.github.com>
2024-05-27 10:40:38 -0500

    ef

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Node.JS-Challenge
+
+# fdf
+
+## Description
+
+fdf
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+dfd
+
+## Usage
+
+dfd
+
+## Contributing
+
+dfd
+
+## Tests
+
+df
+
+## License
+
+Apache 2.0
+
+## Questions
+
+For any questions, please reach out to me via [GitHub](https://github.com/gbdfbd) or [email](mailto:dcscd).
--- index.js
+++ index.js
@@ -1,13 +1,119 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs/promises");
+const path = require("path");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const pathToReadme = path.join(process.cwd(), "README.md");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How to install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How to use your project?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How to contribute to your project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "How to test your project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: [
+      "MIT",
+      "GPLv3",
+      "Apache 2.0",
+      "BSD 2-Clause",
+      "BSD 3-Clause",
+      "MPL 2.0",
+      "None",
+    ],
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+const readmeTemplate = `
+# {{title}}
 
-// Function call to initialize app
-init();
+## Description
+
+{{description}}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+{{installation}}
+
+## Usage
+
+{{usage}}
+
+## Contributing
+
+{{contributing}}
+
+## Tests
+
+{{tests}}
+
+## License
+
+{{license}}
+
+## Questions
+
+For any questions, please reach out to me via [GitHub](https://github.com/{{github}}) or [email](mailto:{{email}}).
+`;
+
+const generateReadmeFile = async ({ questions, readmeTemplate }) => {
+  try {
+    const answers = await inquirer.prompt(questions);
+
+    const readmeContent = readmeTemplate.replace(
+      /{{([^}]+)}}/g,
+      (match, key) => answers[key]
+    );
+
+    await fs.writeFile(pathToReadme, readmeContent, "utf-8");
+  } catch (error) {
+    console.error("Error generating README:", error);
+  }
+};
+
+generateReadmeFile({ questions, readmeTemplate });
